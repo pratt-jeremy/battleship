e6db63c49470433d070b4ad7f84fdc6e064a479a
TaylorDWilliams90 <taytaywilly90@gmail.com>
2014-05-24 18:37:24 -0600

    This class is going to display all of the ships that are going to be used in this game.

---
+++ battleship-master/Battleship1/src/battleship1/GamePreferencesMenuControl.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package battleship1;
+
+import java.awt.Dimension;
+
+/**
+ *
+ * @author jacksonrkj
+ */
+public class GamePreferencesMenuControl {
+    
+    private Game game;
+
+    public GamePreferencesMenuControl(Game game) {
+        this.game = game;
+    }
+    
+    
+    public void getMarker(Player player) { 
+        
+        GetMarkerView getMarkerView = new GetMarkerView(this.game);
+        String marker = getMarkerView.getInput(player);
+        
+        if (marker == null) { // user changed there mind and quit??
+            return;
+        }
+      
+        player.marker = marker; // update the players markers
+        
+        return;
+    }
+        
+    
+    public void getDimensions()  {
+        
+        // Check to see if a game is already in progress
+        if (this.game.status.equals(Game.PLAYING)) {
+            new battleshipError().displayError("You can not change the dimensions "
+              + "of the board once the game has been started. "
+              + "\n\tStart a new game and then change the dimensions "
+              + "of the board. ");
+            return;
+        }
+        
+        GetDimensionsView getDimensionsView = new GetDimensionsView(this.game);
+        Dimension dimension = getDimensionsView.getInput();
+        
+        if (dimension == null) {    
+            return;
+        }
+        
+        int boardRowCount = dimension.width;
+        int boardColumnCount= dimension.height;
+         
+        // no change in the board size so return
+        if (boardRowCount == this.game.board.rowCount &&
+            boardColumnCount == this.game.board.columnCount) {
+            return;
+        }
+        
+        // create new locations in the board
+        this.game.board.createBoardLocations(boardRowCount, boardColumnCount);
+               
+        return;
+    }
+    
+    
+    
+}
---
+++ battleship-master/Battleship1/src/battleship1/GamePreferencesMenuView.java
@@ -0,0 +1,79 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package battleship1;
+
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jacksonrkj
+ */
+
+
+
+
+public class GamePreferencesMenuView  {
+    Game game;
+    private GamePreferencesMenuControl gamePreferenceControl;
+
+    private final static String[][] menuItems = {
+        {"1", "Change Marker of the first Player"},
+        {"2", "Change Marker of the second Player"},
+        {"D", "Change the dimensions of the board"},
+        {"Q", "Return to game menu"}
+    };
+
+    public GamePreferencesMenuView(Game game) {
+        this.game = game;
+        gamePreferenceControl = new GamePreferencesMenuControl(game);
+    }
+
+    
+    public void getInput() {       
+        String command;
+        Scanner inFile = new Scanner(System.in);
+        
+        do {
+            this.display();
+
+            // get commaned entered
+            command = inFile.nextLine();
+            command = command.trim().toUpperCase();
+            
+            switch (command) {
+                case "1":
+                    this.gamePreferenceControl.getMarker(this.game.playerA);
+                    break;
+                case "2":
+                    this.gamePreferenceControl.getMarker(this.game.playerB);
+                    break;
+                case "D":
+                    this.gamePreferenceControl.getDimensions();
+                    break;
+                case "Q":
+                    break;
+                default: 
+                    System.out.println("Invalid command. Please enter a valid command.");
+                    continue;
+            }
+        } while (!command.equals("Q"));
+
+        return;
+    }
+    
+    
+        
+    public final void display() {
+        System.out.println("\n\t===============================================================");
+        System.out.println("\tEnter the letter associated with one of the following commands:");
+
+        for (int i = 0; i < GamePreferencesMenuView.menuItems.length; i++) {
+            System.out.println("\t   " + menuItems[i][0] + "\t" + menuItems[i][1]);
+        }
+        System.out.println("\t===============================================================\n");
+    }
+ 
+}

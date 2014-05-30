1634b66033e27acebe0417491daba1479dbcca9a
Niethy631 <Niethy@192.168.1.2>
2014-05-29 20:17:52 -0700

    MBradshaw Individual Assignment Completed Lesson 4 Assignment 24May2014

---
+++ Battleship1/src/battleship1/GetMarketView.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package battleship1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author mBradshaw
+ */
+public class GetMarketView {
+    
+    private Game game; 
+    
+    /*
+     * default constructor
+     */
+    public GetMarketView(Game game) {
+        this.game = game;
+        
+    } 
+    
+    public String getInput(Players player) {
+
+        String newMarker = null;
+        Scanner in = new Scanner(System.in);
+                
+        boolean valid = false; // flag to indicate if valid character entered
+        while (!valid) {
+            // prompt for input
+            System.out.println("\n\t" + player.name 
+                    + ", enter a single character that will be used to mark "
+                    + "your squares in the game.");
+            
+            // get input from user           
+            newMarker = in.nextLine();
+           
+            // no marker entered?
+            if (newMarker == null  || newMarker.length() < 1) {
+                continue;
+            }
+            
+            // grab only the first character and convert it to upper case
+            newMarker = newMarker.substring(0, 1).toUpperCase();
+            
+            if (newMarker.equals("Q")) { // Quit?
+                return null;
+            }
+            
+            // Check to see if the marker is already in use
+            if (!this.game.playerA.marker.equals(newMarker) &&
+                !this.game.playerB.marker.equals(newMarker) ) {
+            } else {
+                new BattleshipsError().displayError(
+                        "This marker is currently in use. " +
+                                "Select a different character");
+                continue; 
+            }
+            
+            valid = true; // signal that a valid marker was entered
+        }
+        
+        return newMarker;
+    }
+    
+}
+
+
